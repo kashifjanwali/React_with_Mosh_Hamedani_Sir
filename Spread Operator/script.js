6c4a109ed5ebf5a927e82991da52a65c65fd5fee
Kashif Hussain <kh9732494@gmail.com>
2023-08-08 10:02:24 +0500

    ES6 | Spread Operator

--- Spread Operator/script.js
+++ Spread Operator/script.js
@@ -1,17 +1,21 @@
 // Array
 const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
+const array3 = [10, 11, 12];
 
 // Old Method
 // const combined = array1.concat(array2);
+const combined_1 = array1.concat(array2).concat(array3.concat(1,2,3,4,5,6));
+console.log(combined_1);
+
 
 // spread operator
-const combined = [...array1, 7, ...array2, 8, 9, 10]
+const combined = [...array1, 7, ...array2, 8, 9, 10, ...array3];
 console.log(combined);
 
 // Object
 const fName = { first: "KJW" };
 const sName = { sur: "Coding" };
 
-const full = { ...fName, ...sName };
+const full = { sr: 1, ...fName, ...sName, location: "Rawalpindin" };
 console.log(full);
