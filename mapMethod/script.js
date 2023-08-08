99f711d1f28c28bab080c12cc5e8e934c4346230
Kashif Hussain <kh9732494@gmail.com>
2023-08-08 10:48:03 +0500

    ES6 | Array | map()

--- mapMethod/script.js
+++ mapMethod/script.js
@@ -1,10 +1,24 @@
-const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
+// dayNmae is a simple call back function which transform each element in an array
 const dayName = weekDays.map(function (day) {
-    return "<li>" + day + "</li>";
+  return "<li>" + day + "</li>";
 });
 console.log(dayName);
 
+// using arrow functio instead of a simple function
 const colors = ["red", "green", "blue"];
-const items = colors.map(color => `<li>${color}</li>`);
-console.log(items)
+const items = colors.map((color) => "<li>" + color + "</li>");
+console.log(items);
+
+// Template Literal
+const clr_items = colors.map((color) => `<li>${color}</li>`);
+console.log(clr_items);
